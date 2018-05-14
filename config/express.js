2d50f16446c8bdde5f151b9827d40c8f2c337bb8
Guilherme Tognetti <email.guilherme.t@gmail.com>
2018-05-14 12:07:59 -0300

    Adicionado CORS

--- config/express.js
+++ config/express.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(function(req, res, next) {
  	res.header("Access-Control-Allow-Origin", "*");
 	 res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	 res.header("Access-Control-Allow-Origin", "https://guilhermetog.github.io/cli_aluratyper_jquery/");
+	 res.header("Access-Control-Allow-Origin", "https://guilhermetog.github.io");
  	next();
  });
 

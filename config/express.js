a5c587abe28ed2cff849287c580b8041d590742d
Guilherme Tognetti <email.guilherme.t@gmail.com>
2018-05-13 21:32:41 -0300

    Alterado porta do servidor

--- config/express.js
+++ config/express.js
@@ -3,8 +3,6 @@ var consign = require('consign');
 var bodyParser = require('body-parser');
 var app = express();
 
-app.use(express.static('./../public'));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
